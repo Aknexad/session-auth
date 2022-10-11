ea648bccf78cbb952f92918b5da19d63258870ae
Aknexad <aknejad@outlook.com>
2022-10-11 12:44:59 +0330

    add logic

--- 01-query-and-fs/index.js
+++ 01-query-and-fs/index.js
@@ -1,6 +1,19 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 
-http
-  .createServer((req, res) => {})
-  .listen(8080, () => console.log('connect to server'));
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  if (req.query.firstName && req.query.lastName !== 'undefined') {
+    const fullName = `${req.query.firstName} ${req.query.lastName}`;
+    const logger = fs.createWriteStream('log.txt', { flags: 'a' });
+    logger.write(`${fullName},`);
+    res.redirect('/');
+  } else {
+    res.send('Hmoe Page');
+  }
+});
+
+app.listen(8080, () => console.log('conacte to server'));
